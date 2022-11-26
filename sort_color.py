3d1e14b1b91791e9e6957b1e4a0613f9facf854c
tewodrosAe <108419096+tewodrosAe@users.noreply.github.com>
2022-11-26 21:34:08 +0300

    Update sort_color.py

--- sort_color.py
+++ sort_color.py
@@ -1,11 +1,11 @@
 zero = nums.count(0)
-        one = nums.count(1) + zero
-        two = nums.count(2)
-        for i in range(len(nums)):
-            if i < zero:
-                nums[i] = 0
-            else:
-                if i < one:
-                    nums[i] = 1
-                else:
-                    nums[i] = 2
+one = nums.count(1) + zero
+two = nums.count(2)
+for i in range(len(nums)):
+    if i < zero:
+        nums[i] = 0
+    else:
+        if i < one:
+            nums[i] = 1
+        else:
+            nums[i] = 2
